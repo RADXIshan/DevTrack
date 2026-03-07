b247b0d785ad26d43ab4755b6a3b94639cb5a47c
Ishan Roy <ishanroy3118107@gmail.com>
2026-03-07 17:07:14 +0530

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", contactRoute);
+
+app.get("/", (_, res) => {
+  res.send("Server is live!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
